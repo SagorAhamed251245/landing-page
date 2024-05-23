a906eaf5a45def7d08d8bea38035b61c4fba44e0
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-05-23 14:00:55 +0600

    Resources Component added

--- components/FailureReasons/FailureReasons.js
+++ components/FailureReasons/FailureReasons.js
@@ -6,11 +6,13 @@ const FailureReasons = () => {
     "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg";
   const surroundingImages = [a, a, a, a, a, a, a, a];
   return (
-    <section className="failure_reasons container">
+    <section className="failure_reasons">
       <div className="cercel_img">
         <img src="/images/svg/Group 1171283242.png" />
       </div>
+      <div>
       <TextComponent />
+      </div>
       <div style={{ width: "371px", height: "435px" }}>
         <img src="/images/svg/leady.png" />
       </div>
