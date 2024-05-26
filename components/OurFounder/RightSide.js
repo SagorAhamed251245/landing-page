58351fb98093586e007e1fa5d1af97483db4e6e8
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-27 00:50:16 +0600

    our founde section

--- components/OurFounder/RightSide.js
+++ components/OurFounder/RightSide.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 const RightSide = () => {
-  function playPause() {
-    let myVideo = document.getElementById("founder");
-    if (myVideo.paused) myVideo.play();
-    else myVideo.pause();
-  }
   return (
     <div className="founder_info_right">
       <div className="img_div">
@@ -14,7 +9,7 @@ const RightSide = () => {
       <div className="video_card">
         <div className="single_video">
           <div className="div">
-            <video
+            <iframe
               id="founder"
               style={{
                 objectFit: "cover",
@@ -22,25 +17,18 @@ const RightSide = () => {
                 height: "100%",
               }}
               className="video"
-            >
-              <source
-                src="https://videos.pexels.com/video-files/20770858/20770858-hd_1080_1920_30fps.mp4"
-                type="video/mp4"
-              />
-            </video>
+              src="https://www.youtube.com/embed/hNVA_SemREU?si=sFW_n8OVxj7rp8de"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
           <div className="video_relative"></div>
-          <div className="play_button" onClick={playPause}>
-            <div>
-              <img src="/images/svg/Polygon.svg" alt="Play/Pause" />
-            </div>
-          </div>
         </div>
       </div>
-      <img
-        style={{ position: "absolute", bottom: "0", right: "0", zIndex: "-1" }}
-        src="/images/svg/Vector 16.svg"
-      />
+      <img className="yellowVector" src="/images/svg/Vector 16.svg" />
     </div>
   );
 };
