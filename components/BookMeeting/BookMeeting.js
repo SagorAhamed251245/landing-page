a9264e2184ad71f19a37ce6e8a1054e9cbb94984
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-27 02:37:31 +0600

    book meeting

--- components/BookMeeting/BookMeeting.js
+++ components/BookMeeting/BookMeeting.js
@@ -4,23 +4,14 @@ import RightBookMeeting from "./RightBookMeeting";
 
 const BookMeeting = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "#5831F00D",
-        height: "800px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: "120px",
-      }}
-    >
+    <section className="book_meeting_section" style={{}}>
       <div className="container">
         <div className="book_meeting">
           <LeftBookMeeting />
           <RightBookMeeting />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- components/BookMeeting/RightBookMeeting.js
+++ components/BookMeeting/RightBookMeeting.js
@@ -1,39 +1,38 @@
 import React from "react";
 
 const RightBookMeeting = () => {
-  function playPause() {
+  /*  function playPause() {
     let myVideo = document.getElementById("testVideo");
     if (myVideo.paused) myVideo.play();
     else myVideo.pause();
-  }
+  } */
   return (
     <div className="right_book_meeting">
       <div>
         <div className="single_video">
           <div className="div">
-            <video
-              id="testVideo"
+            <iframe
+              id="founder"
               style={{
                 objectFit: "cover",
                 width: "100%",
                 height: "100%",
               }}
               className="video"
-            >
-              <source
-                src={
-                  "https://videos.pexels.com/video-files/1448735/1448735-uhd_4096_2160_24fps.mp4"
-                }
-                type="video/mp4"
-              />
-            </video>
+              src="https://www.youtube.com/embed/hNVA_SemREU?si=sFW_n8OVxj7rp8de"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
-          <div className="video_relative"></div>
+          {/*  <div className="video_relative"></div>
           <div className="play_button" onClick={playPause}>
             <div>
               <img src="/images/svg/Polygon.svg" alt="Play/Pause" />
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="absolute_image"></div>
