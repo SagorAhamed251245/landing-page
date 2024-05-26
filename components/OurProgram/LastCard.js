5569bba4e3589ebb7c402b9989b2bb1f4f19fafb
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-27 04:30:44 +0600

    mearn full stack responsive done

--- components/OurProgram/LastCard.js
+++ components/OurProgram/LastCard.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const LastCard = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "end" }}>
-      <img src="/images/svg/leady.png" />
+    <div
+      className="lastCard"
+      style={{ display: "flex", justifyContent: "end" }}
+    >
+      <div className="div">
+        <iframe
+          id="founder"
+          style={{
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+          }}
+          className="video"
+          src="https://www.youtube.com/embed/hNVA_SemREU?si=sFW_n8OVxj7rp8de"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 };
