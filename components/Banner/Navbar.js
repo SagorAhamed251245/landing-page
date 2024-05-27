6a92cef18d19c15e7b3351206a495c1b19cdf080
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-28 01:02:06 +0600

    navbar done

--- components/Banner/Navbar.js
+++ components/Banner/Navbar.js
@@ -1,26 +1,53 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [mobileNavVisible, setMobileNavVisible] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavVisible(!mobileNavVisible);
+  };
+
   return (
     <nav className="container nav">
       {/* left nav bar */}
       <section className="">
-        <img className="logo" src="/images/svg/logo.svg" />
+        <img className="logo" src="/images/svg/logo.svg" alt="Logo" />
       </section>
       <section className="">
-        <ul>
+        <ul className="desktop_ul">
           <li className="li">Program</li>
           <li className="li">Testimonials</li>
-
           <Link
             target="_blank"
             className="linkBtn li"
-            href={"https://calendly.com/ts4u"}
+            href="https://calendly.com/ts4u"
           >
             Book a meeting
           </Link>
         </ul>
+
+        <section className="mobile_nav_section">
+          <img
+            className="hamburger"
+            src="/images/svg/hamberger.svg"
+            alt="Menu"
+            onClick={toggleMobileNav}
+          />
+          {mobileNavVisible && (
+            <ul className="mobile_ul">
+              <li className="li">Program</li>
+              <li className="li">Testimonials</li>
+              <Link
+                target="_blank"
+                className="linkBtn li"
+                href="https://calendly.com/ts4u"
+              >
+                Book a meeting
+              </Link>
+            </ul>
+          )}
+        </section>
       </section>
     </nav>
   );
