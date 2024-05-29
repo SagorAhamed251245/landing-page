26811fb0bcc6b28cb54310481bd02b93a23d0c71
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-29 13:14:18 +0600

    marquie done

--- components/Alumni/SwiperCard.js
+++ components/Alumni/SwiperCard.js
@@ -1,34 +1,26 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-
-// import required modules
-import { FreeMode, Autoplay } from "swiper/modules";
 import SingleSwiperCard from "./SingleSwiperCard";
 import Marquee from "react-fast-marquee";
 
+// Array of logos
 const logos = [
   "/images/companiLogo/Group 1171281534.png",
   "/images/companiLogo/Group 1171281535.png",
-
   "/images/companiLogo/Group 1171281536.png",
+
   "/images/companiLogo/Group.png",
   "/images/companiLogo/Group 1171281535.png",
   "/images/companiLogo/Group.png",
-
   "/images/companiLogo/Vector.png",
   "/images/companiLogo/walmart 1.png",
+  "/images/companiLogo/Group 1171281536.png",
 ];
 
 const SwiperCard = () => {
   return (
-    <div className="swiper-container container">
-      <div className="Marquee">
-        <Marquee>
+    <div className="swiper-slide-content container">
+      <div className="marquee">
+        <Marquee speed={50} gradient={false}>
           {logos.map((logo, index) => (
             <SingleSwiperCard key={index} url={logo} />
           ))}
--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,6 @@ export default function Home() {
 
           <VehicleValidation />
           <Agile />
-
           {/* <FrontEnd /> */}
           <NextCareer />
           <SuccessfulStudents />
