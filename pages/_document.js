5f9994c65d49d96079b2757621c529b93c1342ba
farukulWD <farukgb1999@gmail.com>
2024-07-15 09:45:43 +0600

    add google tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,21 @@ export default function Document() {
           image={"/images/image/themble.jpg"}
           title={"inquiry.ts4u.us"}
         />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6L2FSQRWYC"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6L2FSQRWYC');
+            `,
+          }}
+        ></script>
       </Head>
 
       <body className="max_container">
@@ -17,27 +32,16 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-
                 !function(f,b,e,v,n,t,s)
-
                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-
                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-
                 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-
                 n.queue=[];t=b.createElement(e);t.async=!0;
-
                 t.src=v;s=b.getElementsByTagName(e)[0];
-
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
-
                 'https://connect.facebook.net/en_US/fbevents.js');
-
                 fbq('init', '318631454217422');
-
                 fbq('track', 'PageView');
-
               `,
           }}
         ></script>
