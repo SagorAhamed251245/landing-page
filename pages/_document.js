3fcc9b3605357a42e2e7da7ce87ec2af3955c501
farukulWD <farukgb1999@gmail.com>
2024-07-25 20:14:18 +0600

    add schema org added

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default function Document() {
 
         <link rel="canonical" href="https://inquiry.ts4u.us/"></link>
 
-        {/** schema.org */}
+        {/** schema.org start*/}
 
         <script
           type="application/ld+json"
@@ -33,7 +33,121 @@ export default function Document() {
             }),
           }}
         />
+        {/* ------------------- */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org/",
+              "@type": "BreadcrumbList",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  name: "Inquiry TS4U",
+                  item: "https://inquiry.ts4u.us/",
+                },
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  name: "",
+                  item: "",
+                },
+              ],
+            }),
+          }}
+        />
 
+        {/* ------------------- */}
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Article",
+              mainEntityOfPage: {
+                "@type": "WebPage",
+                "@id": "https://inquiry.ts4u.us/",
+              },
+              headline:
+                "Transform your career into a six-figure income with TS4U’s Private One-on-One Bootcamp",
+              description:
+                "Transform your career into a six-figure income with TS4U’s Private One-on-One Bootcamp. Discover expert guidance, personalized learning, and a roadmap to success. Learn More!",
+              image:
+                "https://inquiry.ts4u.us/_next/image?url=%2Fimages%2Fsvg%2Flogo.png&w=128&q=75",
+              author: {
+                "@type": "Person",
+                name: "Shiblu Ahmad",
+                url: "https://www.schoolshub.ai/founder",
+              },
+              publisher: {
+                "@type": "Organization",
+                name: "TS4U",
+                logo: {
+                  "@type": "ImageObject",
+                  url: "https://ts4u.us/new-logo/new-logo.svg",
+                },
+              },
+              datePublished: "2024-07-01",
+              dateModified: "2041-12-28",
+            }),
+          }}
+        />
+
+        {/* ------------------- */}
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "ProfessionalService",
+              name: "Private One-on-One Bootcamp",
+              image:
+                "https://inquiry.ts4u.us/_next/image?url=%2Fimages%2Fsvg%2Flogo.png&w=128&q=75",
+              "@id": "",
+              url: "https://inquiry.ts4u.us/",
+              telephone: "+1 (586) 276-7347",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "30500 Van Dyke, Ste- 201",
+                addressLocality: "Warren",
+                addressRegion: "MI",
+                postalCode: "48093",
+                addressCountry: "US",
+              },
+              geo: {
+                "@type": "GeoCoordinates",
+                latitude: 42.51793989999999,
+                longitude: -83.0278453,
+              },
+              openingHoursSpecification: {
+                "@type": "OpeningHoursSpecification",
+                dayOfWeek: [
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday",
+                  "Sunday",
+                ],
+                opens: "00:00",
+                closes: "23:59",
+              },
+              sameAs: [
+                "https://www.facebook.com/ts4u.us",
+                "https://www.instagram.com/ts4uservices/",
+                "https://www.youtube.com/@ts4uITbootcamp",
+                "https://www.linkedin.com/company/31284119/",
+                "https://inquiry.ts4u.us/",
+              ],
+            }),
+          }}
+        />
+
+        {/** schema.org end*/}
         {/**keyword */}
 
         <meta
