e1174755e3fd6944fafe2f15779f303b6c7fdd86
farukulWD <farukgb1999@gmail.com>
2024-09-02 18:15:56 +0600

    add image on modal

--- components/HomePopup/HomePopup.js
+++ components/HomePopup/HomePopup.js
@@ -43,7 +43,7 @@ function HomePopup() {
           >
             <img
               className="modal-img"
-              src="/home_popup/e_com_project.jpg"
+              src="/home_popup/learn_lenux.jpg"
               alt="modal"
             />
           </a>
